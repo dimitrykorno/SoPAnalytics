99a8a4119492a45d47c89192426dba6700959701
dimitrykorno <32943435+dimitrykorno@users.noreply.github.com>
2018-11-28 18:00:35 +0300

    дорпаботка отчета по прогрессу, выделение на старт/фейл/финиш

--- Reports/ProgressHistogram.py
+++ Reports/ProgressHistogram.py
@@ -4,8 +4,10 @@ from itertools import chain
 from Classes.Events import *
 from Data import Parse
 from Classes.User import User
+from Classes.Events import *
 from report_api.Report import Report
-from report_api.Utilities.Utils import time_count
+from report_api.Utilities.Utils import time_count, draw_plot
+from Utilities.Quests import loc_quest_level, get_locquest
 
 app = "sop"
 
@@ -13,20 +15,33 @@ app = "sop"
 # noinspection PyDefaultArgument,PyDefaultArgument,PyDefaultArgument,PyDefaultArgument
 @time_count
 def new_report(os_list=["iOS"],
-               max_level=121,
-               days=[2, 3],
-               app_version=["6.0", "6.3"],
-               period_start=None,
-               period_end=None,
+               max_level=60,
+               days=[0, 1, 2, 3, 4],
+               app_version=["7.5"],
+               period_start="2018-11-15",
+               period_end="2018-11-18",
                countries_list=[],
                users_last_day=False):
     days = list(map(int, days))
     if not isinstance(app_version, list):
         app_version = list(app_version)
-    progress = {}
+    progress_levels = {}
+    progress_quests = {}
     users = dict.fromkeys(app_version, 0)
     # с 1 (0, чтобы точнее были деления) по n (включительно) + 3 финишера (15?)
     levels = list(range(0, max_level + 2))
+    quests = []
+    end = False
+    for location in loc_quest_level:
+        for quest in loc_quest_level[location]:
+            if str(max_level).rjust(4, '0') not in loc_quest_level[location][quest]:
+                quests.append(location + quest)
+            else:
+                quests.append(location + quest)
+                end = True
+                break
+        if end:
+            break
 
     for os_str in os_list:
         for version in app_version:
@@ -45,100 +60,85 @@ def new_report(os_list=["iOS"],
                                    period_end=None,
                                    min_version=None,
                                    max_version=None,
-                                   events_list=[("Match3Events", ["%CompleteTargets%", "%FinishGame%"])])
-            # ПАРАМЕТРЫ
+                                   events_list=[("Match3Events",
+                                                 ["%CompleteTargets%", "%StartGame%", "%FailGame%", "%FinishGame%"]),
+                                                ("CityEvent", "%InitGameState"),
+                                                ("",
+                                                 ["%Match3Events%CompleteTargets%", "%Match3Events%StartGame%",
+                                                  "%Match3Events%FailGame%", "%Match3Events%FinishGame%"]),
+                                                ("", "%CityEvent%InitGameState%")])
 
-            finished_levels = set()
+            # ПАРАМЕТРЫ
             previous_day = None
-            progress[version] = {}
+            progress_levels[version] = {}
+            progress_quests[version] = {}
             for day in days:
-                progress[version][day] = [0] * len(levels)
+                progress_levels[version][str(day) + " day"] = []
+                progress_quests[version][str(day) + " day"] = [0] * len(quests)
+                for _ in levels:
+                    progress_levels[version][str(day) + " day"].append([0, 0, 0])
+
+            previous_m3_event = None if users_last_day else Match3StartGame(level_num="0001")
+            last_updated_day = 0
+
+            def add_event(event, d):
+                level = int(event.level_num)
+                if level < max_level:
+                    if isinstance(event, Match3CompleteTargets) \
+                            or isinstance(event, Match3FinishGame):
+                        progress_levels[version][str(d) + " day"][level][2] += 1
+                    elif isinstance(event, Match3StartGame):
+                        quest = get_locquest(str(level).rjust(4, '0'))
+                        progress_quests[version][str(d) + " day"][quests.index(quest)] += 1
+                        progress_levels[version][str(d) + " day"][level][0] += 1
+                    elif isinstance(event, Match3FailGame):
+                        progress_levels[version][str(d) + " day"][level][1] += 1
+
+            def add_user_progress():
+                if not users_last_day:
+                    for day in [d for d in range(last_updated_day, day_in_game) if d in days]:
+                        add_event(previous_m3_event, day)
+
+            def flush_user_info():
+                if previous_day in days:
+                        add_event(previous_m3_event, previous_day)
+                if not users_last_day:
+                    for day in [d for d in days if d > previous_day]:
+                        add_event(previous_m3_event, day)
 
             while Report.get_next_event():
-
                 day_in_game = Report.get_time_since_install(measure="day")
-                if Report.is_new_user() or (not users_last_day and (previous_day and day_in_game != previous_day)):
-                    # НЕ КАЖДОЕ СОБЫТИЕ А ТОЛЬКО КОГДА МЕНЯЕТСЯ ДЕНЬ,
-                    #  либо только когда меняется пользователь, если только для тех, у кого это последний день
-                    if previous_day in days and finished_levels:
-                        max_user_level = int(max(finished_levels))
 
-                        # учитываем финишеры
-                        if max_user_level < 9000:
-                            progress[version][previous_day][min(max_user_level, max_level)] += 1
-                        else:
-                            progress[version][previous_day][max_level + 1] += 1
-
+                # НЕ КАЖДОЕ СОБЫТИЕ А ТОЛЬКО КОГДА МЕНЯЕТСЯ ДЕНЬ,
+                #  либо только когда меняется пользователь, если только для тех, у кого это последний день
                 if Report.is_new_user():
-                    finished_levels = set()
+                    if previous_m3_event:
+                        flush_user_info()
+                    previous_m3_event = None if users_last_day else Match3StartGame(level_num="0001")
+                    last_updated_day = 0
 
-                finished_levels.add(Report.current_event.level_num)
+                elif not users_last_day and previous_day is not None and day_in_game != previous_day:
+                    add_user_progress()
+                    last_updated_day = max(max([0] + [d for d in range(last_updated_day, day_in_game) if d in days]) + 1,last_updated_day)
+
+                if issubclass(Report.current_event.__class__, Match3Event):
+                    previous_m3_event = Report.current_event
                 previous_day = day_in_game
-            print(version, "Total users:", Report.total_users)
+
+            if previous_m3_event:
+                flush_user_info()
+
+            print("Version", version, "Total users:", Report.total_users)
             users[version] = Report.total_users
+            for day in days:
+                print("day:", day, progress_levels[version][str(day) + " day"])
 
-        # Рисовка гистограммы
-        max_on_screen = 4
-        columns = min(2, len(days))
-        # columns=2
-        lines_wanted = 2
-        plots_left = len(days)
-        screens = int(math.ceil(plots_left / max_on_screen))
-        for i in range(screens):
-
-            lines = lines_wanted if plots_left >= max_on_screen else math.ceil(plots_left / columns)
-            fig, axs = plt.subplots(lines, columns, figsize=(18, 8), facecolor='w', edgecolor='k')
-            fig.subplots_adjust(hspace=.5, wspace=.2)
-            if columns > 1:
-                axs = axs.ravel()
-
-            for subplot, day in enumerate(days[i * max_on_screen: min((i + 1) * max_on_screen, len(days))]):
-                if columns > 1:
-                    ax = axs[subplot]
-                else:
-                    ax = axs
-                for version in app_version:
-                    ax.bar(levels, progress[version][day], alpha=0.5, label=version)
-                    ax.legend()
-                    ax.set_xticks(list(chain(range(0, max_level + 1, 10), [max_level + 2])))
-                    ax.set_xticklabels(list(range(0, max_level + 1, 10)) + ["X"])
-                    ax.set_title("day " + str(day))
-            plt.savefig("progress_histo_absolute" + str(app_version) + " (" + str(i) + ").png")
-            plt.show()
-
-            plots_left -= columns * lines
-
-            # Рисовка гистограммы
-            max_on_screen = 4
-            columns = min(2, len(days))
-            # columns = 2
-            lines_wanted = 2
-            plots_left = len(days)
-            screens = int(math.ceil(plots_left / max_on_screen))
-            for j in range(screens):
-
-                lines = lines_wanted if plots_left >= max_on_screen else math.ceil(plots_left / columns)
-                fig, axs = plt.subplots(lines, columns, figsize=(18, 8), facecolor='w', edgecolor='k')
-                fig.subplots_adjust(hspace=.5, wspace=.2)
-
-                if columns > 1:
-                    axs = axs.ravel()
-
-                for subplot, day in enumerate(days[i * max_on_screen: min((j + 1) * max_on_screen, len(days))]):
-                    if columns > 1:
-                        ax = axs[subplot]
-                    else:
-                        ax = axs
-                    for version in app_version:
-                        ax.bar(levels,
-                               [round(user_count * 100 / users[version], 1) for user_count in progress[version][day]],
-                               alpha=0.5, label=version)
-                        ax.legend()
-                        ax.set_xticks(list(chain(range(0, max_level + 1, 10), [max_level + 2])))
-                        ax.set_xticklabels(list(range(0, max_level + 1, 10)) + ["X"])
-                        ax.set_title("day " + str(day))
-                plt.savefig("Results/Гистограма прогресса/progress_histo " + str(app_version) +
-                            " (" + str(j) + ")" + " " + os_str + ".png")
-                plt.show()
-
-                plots_left -= columns * lines
+        labels = {}
+        for d in days:
+            labels[str(d) + " day"] = ["Start", "Fail", "Finish"]
+        title = "Гистограма прогресса" + str(app_version) + " " + str(period_start) + "-" + str(
+            period_end)
+        if users_last_day:
+            title += " last day"
+        draw_plot(levels, progress_levels, additional_labels=labels, show=True, colors=["y", "r", "g"], title=title,
+                  size=(2, 1), folder="Results/Гистограма прогресса/")
